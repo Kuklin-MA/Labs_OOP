bd0d5824f0e68f45a7491e92157526ac040c76b7
Matvey <146710282+Kuklin-MA@users.noreply.github.com>
2024-12-19 23:57:56 +0300

    Add files via upload

--- complex.cpp
+++ complex.cpp
@@ -4,7 +4,7 @@
 #include <cmath>
 using namespace std;
 ///Эта функция очищает поток ввода, если пользователь
-///вводит ерись, когда его просят ввести что-то с консоли.
+///вводит ересь, когда его просят ввести что-то с консоли.
 void cl(){
 if (std::cin.fail())std::cin.clear();// проверяем состояние потока и сбрасываем если сломан
         cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // сбрасываем буфер
@@ -14,10 +14,17 @@ class Complex{
 private:
     double real;
     double imeg;
+    double mdls (){
+    return sqrt(pow(real,2)+pow(imeg,2));
+    }
+//    double mdls (double real,double imeg){
+//    return sqrt(pow(real,2)+pow(imeg,2));
+//    }
 public:
 ///Конструкторы
 Complex(): real(1), imeg(1){}
 Complex(double r,double i): real(r), imeg(i){}
+Complex(double i): real(i), imeg(0){}
 //#####################################################
 
 ///Перегрузка cin
@@ -47,13 +54,7 @@ return strm;
 
 //#####################################################
 /// Арифметика
-//C целым числом
-  Complex operator+ (int n) const{return Complex(real + n,imeg);}
-  Complex operator- (int n) const{return Complex(real - n,imeg);}
-  Complex operator* (int n) const{return Complex(real * n,imeg * n);}
-  Complex operator/ (int n) const{return Complex(real/n,imeg/n);}
-
-//C вещественным числом
+//C вещественным(целым) числом
   Complex operator+ (double n) const{return Complex(real + n,imeg);}
   Complex operator- (double n) const{return Complex(real - n,imeg);}
   Complex operator* (double n) const{return Complex(real * n,imeg * n);}
@@ -75,15 +76,123 @@ return strm;
                       (imeg*cx2.real - real*cx2.imeg)/denum );
   }
 
+//#####################################################
+/// Логика
+//С вещественным(целым) числом
+bool operator> (double n){return this->mdls()>n;}
+bool operator< (double n){return this->mdls()<n;}
+bool operator>= (double n){return this->mdls()>=n;}
+bool operator<= (double n){return this->mdls()<=n;}
+bool operator== (double n){return this->mdls()==n;}
+bool operator!= (double n){return this->mdls()!=n;}
+
+//С комплексным числом
+bool operator> (Complex cx2){return this->mdls()> cx2.mdls();}
+bool operator< (Complex cx2){return this->mdls()< cx2.mdls();}
+bool operator>= (Complex cx2){return this->mdls()>= cx2.mdls();}
+bool operator<= (Complex cx2){return this->mdls()<= cx2.mdls();}
+bool operator==(Complex cx2){return this->mdls()== cx2.mdls();}
+bool operator!=(Complex cx2){return this->mdls()!=cx2.mdls();}
+
+//#####################################################
+///Инкрементация
+////Префиксная
+Complex operator++ (){
+return real++;
+}
+////Постфиксная
+Complex operator++ (int){
+return imeg++;
+}
+
+///Дикрементация
+////Префиксная
+Complex operator-- (){
+return real--;
+}
+////Постфиксная
+Complex operator-- (int){
+return imeg--;
+}
+
+//#####################################################
+///Сопряжённое число (conjugate number)
+Complex get_conj_number(Complex cx1){return Complex(real,imeg*-1);}
+
+//#####################################################
+///Вывести в тригонометрической форме (Trigonometry)
+void out_trig (){
+    // acos(-1.0) - это pi
+    if(real==0)
+        if(imeg<0)cout<<this->mdls()<<"*(cos("<< -acos(-1.0)/2
+                                  <<") + isin("<<-acos(-1.0)/2<<"))"<<endl;
+
+        else
+        cout<<this->mdls()<<"*(cos("<< acos(-1.0)/2
+                                  <<") + isin("<<acos(-1.0)/2<<"))"<<endl;
+
+
+    else
+        if(real>0)
+        cout<<this->mdls()<<"*(cos("<<atan(imeg/real)
+            <<") + isin("<<atan(imeg/real)<<"))"<<endl;
+
+        else cout<<this->mdls()<<"*(cos("<<atan(imeg/real) - acos(-1.0)
+                 <<") + isin("<<atan(imeg/real) - acos(-1.0)<<"))"<<endl;
+
+
+}
+
+//#####################################################
+///Возвести в степень (Raise to the power)
+Complex cx_pow (double n){
+    if(real==0)
+        if(fmod(n,2)==0) return Complex(pow(this->mdls(),n)*-1,0);
+        else return Complex(0,pow(this->mdls(),n));
+    else
+        if(real>0)
+        return Complex (pow(this->mdls(),n)*cos(atan(imeg/real)*n),
+               pow(this->mdls(),n)*sin(atan(imeg/real)*n));
+
+        // acos(-1.0) - это pi
+        else
+        return Complex(pow(this->mdls(),n)*cos((atan(imeg/real) - acos(-1.0))*n),
+               pow(this->mdls(),n)*sin((atan(imeg/real) - acos(-1.0))*n));
+}
+//#####################################################
+///Извлечение корня n-степени (sqrt без s)
+Complex cx_qrt (double n){
+    if(real==0)
+        if(imeg>0) return Complex(pow(this->mdls(),1/n)*cos(acos(-1.0)/(2*n)),
+                                  pow(this->mdls(),1/n)*sin(acos(-1.0)/(2*n)) );
+        else return Complex(pow(this->mdls(),1/n)*cos(-acos(-1.0)/(2*n)),
+                            pow(this->mdls(),1/n)*sin(-acos(-1.0)/(2*n)) );
+    else return this->cx_pow(1/n);
+}
+//#####################################################
+///Вывести показательной  форме (the exponential form of a number)
+void out_exp(){
+    if(real==0)
+        if(imeg>0)cout<<this->mdls()<<"e^i"<<acos(-1.0)/2;
+        else cout<<this->mdls()<<"e^i"<<-acos(-1.0)/2;
+    else
+        if(real>0)cout<<this->mdls()<<"e^i"<<atan(imeg/real);
+        else cout<<this->mdls()<<"e^i"<<atan(imeg/real) - acos(-1.0);
+}
 };
+
 
 int main()
 {
-Complex cx1, cx2(3, -5);
+Complex cx1, cx2(3,-4);
 cout<<cx1<<" | "<<cx2<< endl;
 cout<<"1) "<< cx2 + cx1 << endl;
 cout<<"2) "<< cx2 - cx1 << endl;
 cout<<"3) "<< cx2 * cx1 << endl;
 cout<<"4) "<< cx2 / cx1 << endl;
+cout<<"5) "; cx2.out_trig();
+cout<<"6) "<< cx2.cx_pow(2)<<endl;
+cout<<"7) "<< cx2.cx_qrt(2)<<endl;
+cout<<"8) "; cx2.out_exp();
     return 0;
 }
